16d51caf73b37ff12090fb53764b425b59ce5adf
Neiki <philippeckert.1@gmx.de>
2023-07-24 20:04:28 +0200

    fixed bug

--- src/scripts/custom-buttons.user.js
+++ src/scripts/custom-buttons.user.js
@@ -138,6 +138,7 @@
       } else return true;
    }
    function getOpenTipData(sha256Hash) {
+      if (localStorage.getItem("opentip-data")) return;
       if (!checkOpenTipKeyStatus()) {
          const resetKey = confirm("No or invalid Key found!\nDo you want to reset the local stored key?")
          if (resetKey) { deleteCookie("opentip_api_key"); alert("Please reload the page, to enter a new key!") }
