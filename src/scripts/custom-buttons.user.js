a8fe61eba7c3e27b1421ebd55fb1ac6bd91ad367
Neiki <philippeckert.1@gmx.de>
2023-07-25 17:39:39 +0200

    update, added button instead of alert box

--- src/scripts/custom-buttons.user.js
+++ src/scripts/custom-buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VirusTotal Custom Buttons
 // @namespace    http://tampermonkey.net/
-// @version      1.1.7
+// @version      1.1.9
 // @description  adds custom buttons and also (optional) loading opentip data report 
 // @author       NeikiDev
 // @match        https://www.virustotal.com/gui/file/*
@@ -12,7 +12,7 @@
 
 (function () {
    'use strict';
-   setTimeout(() => {addButtons();}, 3000)
+   setTimeout(() => { addButtons(); }, 3000)
    setInterval(() => {
       if (document.querySelector("file-view")
          && document.querySelector("file-view").shadowRoot.getElementById("report")
@@ -27,7 +27,9 @@
       let sha256Hash = hashRegex.exec(window.location.href);
       if (!sha256Hash || !sha256Hash[0]) return;
       sha256Hash = sha256Hash[0].toLowerCase();
-      getOpenTipData(sha256Hash);
+      if (!localStorage.getItem("opentip-data")) {
+         addOpenTipStartDiv(sha256Hash);
+      }
       if (document.querySelector("file-view") && document.querySelector("file-view").shadowRoot.getElementById("report") && document.querySelector("file-view").shadowRoot.getElementById("report").shadowRoot.querySelector(".nav")) {
          const buttons_to_add = getButtons(sha256Hash)
          document.querySelector("file-view").shadowRoot.getElementById("report").shadowRoot.querySelector(".nav").innerHTML += buttons_to_add
@@ -72,6 +74,26 @@
    function getSettingsCode() {
       return 'const e=prompt("ENTER YOUR OPENTIP APIKEY\\nNote: The key is stored (cookie) locally in your Browser!");e?(document.cookie=`opentip_api_key=${encodeURIComponent(e)};path=/`,alert("Done, please reload your browser!")):alert("Failed, please try again!");'
    }
+   function addOpenTipStartDiv(sha256Hash) {
+      const newDiv = document.createElement("div");
+      newDiv.innerHTML = `
+      <div class="popular-threat-name border border-top-0 mb-2 p-3 hstack gap-4 bg-body-secondary" style="margin-top: -5px;">
+      <div class="col hstack gap-2">
+         <span class="fw-bold">Kaspersky Opentip Analysis</span> 
+         <div class="badge rounded-pill bg-body-tertiary text-body">
+         <span id="start_opentip" class="fw-bold">Click to start!</span> 
+      </div>
+      </div>
+      <div class="badge rounded-pill bg-body-tertiary text-body">
+         <span onclick='${getSettingsCode()}' class="fw-bold">change apikey</span> 
+      </div>
+   </div>`
+      const containerDiv = document.querySelector("file-view").shadowRoot.getElementById("report").querySelector(".tab-slot");
+      containerDiv.insertBefore(newDiv, containerDiv.firstChild)
+      document.querySelector("file-view").shadowRoot.getElementById("report").querySelector(".tab-slot").querySelector("#start_opentip").addEventListener("click", e => {
+         getOpenTipData(sha256Hash)
+      })
+   }
    function addOpenTipDivLoader(sha256Hash) {
       const newDiv = document.createElement("div");
       newDiv.innerHTML = `
@@ -95,6 +117,7 @@
       </div>
    </div>`
       const containerDiv = document.querySelector("file-view").shadowRoot.getElementById("report").querySelector(".tab-slot");
+      containerDiv.removeChild(containerDiv.firstChild)
       containerDiv.insertBefore(newDiv, containerDiv.firstChild)
    }
    function addOpenTipDivError(error_message) {
@@ -163,13 +186,12 @@
       return null;
    }
    function getOpenTipData(sha256Hash) {
-      if (localStorage.getItem("opentip-data")) return;
-      localStorage.setItem("opentip-data", "already-fetched")
-      if (!confirm("Do you want to fetch the Kaspersky Opentip Report data?")) return;
       if (!getCookieValue("opentip_api_key")) {
          addOpenTipDivError("Missing APIKEY!");
          console.log("Please use the settings button to add or change your key!")
       } else {
+         if (localStorage.getItem("opentip-data")) return;
+         localStorage.setItem("opentip-data", "already-fetched")
          addOpenTipDivLoader()
          fetch(`https://proxy.pleasedontbearealdomain.com/https://opentip.kaspersky.com/api/v1/getresult/file?request=${sha256Hash}`, {
             method: "POST",
