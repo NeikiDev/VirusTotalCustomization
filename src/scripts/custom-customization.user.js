4552923b4f14f1ffdf8e9f9148fed7935e7d8f8e
Neiki <philippeckert.1@gmx.de>
2023-07-25 19:55:33 +0200

    new version 2.0.0 - updated and renamed

--- src/scripts/custom-customization.user.js
+++ src/scripts/custom-customization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VirusTotal Custom Buttons
 // @namespace    http://tampermonkey.net/
-// @version      1.2.0
+// @version      2.0.0
 // @description  adds custom buttons and also (optional) loading opentip data report 
 // @author       NeikiDev
 // @match        https://www.virustotal.com/gui/file/*
