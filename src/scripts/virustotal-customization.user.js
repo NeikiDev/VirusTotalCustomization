5b206a774288485e7f6bc50dd0971a57b6e8c072
Neiki <philippeckert.1@gmx.de>
2024-01-02 21:10:03 +0100

    Update VirusTotal customization version and report URL

--- src/scripts/virustotal-customization.user.js
+++ src/scripts/virustotal-customization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VirusTotal customization
 // @namespace    http://tampermonkey.net/
-// @version      2.3.4
+// @version      2.3.5
 // @description  VirusTotal customization plugin - buttons and more
 // @author       NeikiDev
 // @match        https://www.virustotal.com/gui/file/*
@@ -72,7 +72,7 @@
         </li>
         <li class="nav-item" role="presentation">
            <a data-bs-toggle="tab" role="tab" no-history="" class="nav-link p-3 px-4  hstack gap-2" aria-selected="false" data-route="community"
-              href="https://analyze.neiki.dev/generate?hash=${sha256Hash}" target="_blank">
+              href="https://analyze.neiki.dev/reports/${sha256Hash}" target="_blank">
               <span>
                  <!---->Neiki<!---->
               </span>
